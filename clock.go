ddc2637308f518063cc24fa112d57ceb5cc7d09c
Niels Widger <niels.widger@gmail.com>
2014-01-07 17:56:32 -0500

    Add comments to functions in clock.go

--- clock.go
+++ clock.go
@@ -4,6 +4,9 @@ import (
 	"time"
 )
 
+// Represents a clock signal for an IC.  Once a Clock is started, it
+// maintains a 'ticks' counters which is incremented at a specific
+// interval.
 type Clock struct {
 	rate     time.Duration
 	ticks    uint64
@@ -12,6 +15,9 @@ type Clock struct {
 	waiting  map[uint64][]chan int
 }
 
+// Returns a pointer to a new Clock which increments its ticker at
+// intervals of 'rate'.  The returned Clock has not been started and
+// its ticks counter is zero.
 func NewClock(rate time.Duration) *Clock {
 	return &Clock{
 		rate:     rate,
@@ -42,10 +48,12 @@ func (clock *Clock) maintainTime() {
 	}
 }
 
+// Returns the current value of the Clock's ticks counter.
 func (clock *Clock) Ticks() uint64 {
 	return clock.ticks
 }
 
+// Starts the Clock
 func (clock *Clock) Start() (ticks uint64) {
 	ticks = clock.ticks
 
@@ -57,12 +65,15 @@ func (clock *Clock) Start() (ticks uint64) {
 	return
 }
 
+// Stops the clock
 func (clock *Clock) Stop() {
 	if clock.ticker != nil {
 		clock.stopChan <- 1
 	}
 }
 
+// Blocks the calling thread until the given tick has arrived.
+// Returns immediately if the clock has already passed the given tick.
 func (clock *Clock) Await(tick uint64) uint64 {
 	if clock.ticks < tick {
 		C := make(chan int, 1)
